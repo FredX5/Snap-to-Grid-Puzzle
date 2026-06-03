1b3f53414f8bed0347d08f81485c9aadacd4bf4c
Farid Jamalov <faridjamalov006@gmail.com>
2026-06-03 18:31:57 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.btnCreatePuzzle = new System.Windows.Forms.Button();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.cmbGridSize = new System.Windows.Forms.ComboBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlSource = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlTarget = new System.Windows.Forms.TableLayoutPanel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectImage.Location = new System.Drawing.Point(194, 23);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(152, 35);
+            this.btnSelectImage.TabIndex = 0;
+            this.btnSelectImage.Text = "Resim Sec";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // btnCreatePuzzle
+            // 
+            this.btnCreatePuzzle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreatePuzzle.Location = new System.Drawing.Point(194, 71);
+            this.btnCreatePuzzle.Name = "btnCreatePuzzle";
+            this.btnCreatePuzzle.Size = new System.Drawing.Size(152, 40);
+            this.btnCreatePuzzle.TabIndex = 1;
+            this.btnCreatePuzzle.Text = "Puzzle Oluştur";
+            this.btnCreatePuzzle.UseVisualStyleBackColor = true;
+            this.btnCreatePuzzle.Click += new System.EventHandler(this.btnCreatePuzzle_Click);
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartGame.Location = new System.Drawing.Point(381, 76);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(158, 30);
+            this.btnStartGame.TabIndex = 2;
+            this.btnStartGame.Text = "Oyunu Başlat";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // cmbGridSize
+            // 
+            this.cmbGridSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbGridSize.FormattingEnabled = true;
+            this.cmbGridSize.Location = new System.Drawing.Point(370, 23);
+            this.cmbGridSize.Name = "cmbGridSize";
+            this.cmbGridSize.Size = new System.Drawing.Size(121, 26);
+            this.cmbGridSize.TabIndex = 3;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(632, 64);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(69, 29);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "Skor:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(632, 23);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(83, 29);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "zaman";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnlSource
+            // 
+            this.pnlSource.Location = new System.Drawing.Point(12, 183);
+            this.pnlSource.Name = "pnlSource";
+            this.pnlSource.Size = new System.Drawing.Size(303, 350);
+            this.pnlSource.TabIndex = 6;
+            // 
+            // pnlTarget
+            // 
+            this.pnlTarget.ColumnCount = 2;
+            this.pnlTarget.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.pnlTarget.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.pnlTarget.Location = new System.Drawing.Point(397, 183);
+            this.pnlTarget.Name = "pnlTarget";
+            this.pnlTarget.RowCount = 2;
+            this.pnlTarget.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.pnlTarget.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.pnlTarget.Size = new System.Drawing.Size(376, 350);
+            this.pnlTarget.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // picPreview
+            // 
+            this.picPreview.Location = new System.Drawing.Point(12, 12);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(120, 90);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 8;
+            this.picPreview.TabStop = false;
+            this.picPreview.Click += new System.EventHandler(this.picPreview_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 571);
+            this.Controls.Add(this.picPreview);
+            this.Controls.Add(this.pnlTarget);
+            this.Controls.Add(this.pnlSource);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.cmbGridSize);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.btnCreatePuzzle);
+            this.Controls.Add(this.btnSelectImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.Button btnCreatePuzzle;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.ComboBox cmbGridSize;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FlowLayoutPanel pnlSource;
+        private System.Windows.Forms.TableLayoutPanel pnlTarget;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox picPreview;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap originalImage;
+
+        int gridSize = 2;
+        int score = 0;
+        int timeLeft = 0;
+        int totalTime = 120;
+        bool gameStarted = false;
+
+        Bitmap[,] correctPieces;
+        int[,] correctIndexes;
+        int[,] userBoard;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbGridSize.Items.Add("2x2");
+            cmbGridSize.Items.Add("4x4");
+            cmbGridSize.SelectedIndex = 0;
+
+            lblScore.Text = "Score: 0";
+            lblTime.Text = "Time: 0";
+
+            timer1.Interval = 1000;
+
+            pnlSource.AutoScroll = true;
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog1.FileName);
+                picPreview.Image = originalImage;
+                MessageBox.Show("Resim seçildi.");
+            }
+        }
+
+        private void btnCreatePuzzle_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Önce resim seçin.");
+                return;
+            }
+
+            gameStarted = false;
+            timer1.Stop();
+
+            string selectedValue = cmbGridSize.SelectedItem.ToString();
+            gridSize = int.Parse(selectedValue.Split('x')[0]);
+
+            SliceImage();
+            InitializeArrays();
+            CreateTargetGrid();
+            ShuffleAndShowPieces();
+
+            lblScore.Text = "Score: 0";
+            lblTime.Text = "Time: 0";
+
+            MessageBox.Show("Puzzle oluşturuldu.");
+        }
+
+        private void InitializeArrays()
+        {
+            correctIndexes = new int[gridSize, gridSize];
+            userBoard = new int[gridSize, gridSize];
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    int index = row * gridSize + col;
+                    correctIndexes[row, col] = index;
+                    userBoard[row, col] = -1;
+                }
+            }
+        }
+
+        private void SliceImage()
+        {
+            correctPieces = new Bitmap[gridSize, gridSize];
+
+            int pieceWidth = originalImage.Width / gridSize;
+            int pieceHeight = originalImage.Height / gridSize;
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    Bitmap piece = new Bitmap(pieceWidth, pieceHeight);
+
+                    using (Graphics g = Graphics.FromImage(piece))
+                    {
+                        g.DrawImage(
+                            originalImage,
+                            new Rectangle(0, 0, pieceWidth, pieceHeight),
+                            new Rectangle(col * pieceWidth, row * pieceHeight, pieceWidth, pieceHeight),
+                            GraphicsUnit.Pixel
+                        );
+                    }
+
+                    correctPieces[row, col] = piece;
+                }
+            }
+        }
+
+        private void CreateTargetGrid()
+        {
+            pnlTarget.Controls.Clear();
+            pnlTarget.RowStyles.Clear();
+            pnlTarget.ColumnStyles.Clear();
+
+            pnlTarget.RowCount = gridSize;
+            pnlTarget.ColumnCount = gridSize;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                pnlTarget.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / gridSize));
+                pnlTarget.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / gridSize));
+            }
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    Panel cell = new Panel();
+                    cell.Dock = DockStyle.Fill;
+                    cell.BorderStyle = BorderStyle.FixedSingle;
+                    cell.AllowDrop = true;
+                    cell.Tag = new Point(row, col);
+
+                    cell.DragEnter += Cell_DragEnter;
+                    cell.DragDrop += Cell_DragDrop;
+
+                    pnlTarget.Controls.Add(cell, col, row);
+                }
+            }
+        }
+
+        private void ShuffleAndShowPieces()
+        {
+            pnlSource.Controls.Clear();
+
+            int totalPieces = gridSize * gridSize;
+
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < totalPieces; i++)
+            {
+                indexes.Add(i);
+            }
+
+            Random rnd = new Random();
+            indexes = indexes.OrderBy(x => rnd.Next()).ToList();
+
+            for (int i = 0; i < totalPieces; i++)
+            {
+                int pieceIndex = indexes[i];
+                int row = pieceIndex / gridSize;
+                int col = pieceIndex % gridSize;
+
+                PictureBox pb = new PictureBox();
+                pb.Width = 80;
+                pb.Height = 80;
+                pb.BorderStyle = BorderStyle.FixedSingle;
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Image = correctPieces[row, col];
+                pb.Tag = pieceIndex;
+                pb.MouseDown += Piece_MouseDown;
+
+                pnlSource.Controls.Add(pb);
+            }
+        }
+
+        private void Piece_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!gameStarted)
+            {
+                MessageBox.Show("Önce oyunu başlat.");
+                return;
+            }
+
+            PictureBox pb = sender as PictureBox;
+
+            if (pb != null)
+            {
+                pb.DoDragDrop(pb, DragDropEffects.Move);
+            }
+        }
+
+        private void Cell_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void Cell_DragDrop(object sender, DragEventArgs e)
+        {
+            if (!gameStarted) return;
+
+            PictureBox dragged = e.Data.GetData(typeof(PictureBox)) as PictureBox;
+            Panel cell = sender as Panel;
+
+            if (dragged == null || cell == null)
+                return;
+
+            int row = pnlTarget.GetRow(cell);
+            int col = pnlTarget.GetColumn(cell);
+
+            int pieceIndex = (int)dragged.Tag;
+
+            if (userBoard[row, col] != -1)
+            {
+                MessageBox.Show("Bu hücre dolu.");
+                return;
+            }
+
+            if (pieceIndex == correctIndexes[row, col])
+            {
+                userBoard[row, col] = pieceIndex;
+
+                pnlSource.Controls.Remove(dragged);
+                dragged.Dock = DockStyle.Fill;
+                cell.Controls.Add(dragged);
+
+                score += 50;
+            }
+            else
+            {
+                score -= 10;
+                MessageBox.Show("Yanlış yer!");
+            }
+
+            lblScore.Text = "Score: " + score;
+            CheckGameCompleted();
+        }
+
+        private void CheckGameCompleted()
+        {
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    if (userBoard[row, col] != correctIndexes[row, col])
+                    {
+                        return;
+                    }
+                }
+            }
+
+            timer1.Stop();
+            gameStarted = false;
+
+            int usedTime = totalTime - timeLeft;
+
+            MessageBox.Show(
+                "Tebrikler! Puzzle tamamlandı.\n" +
+                "Geçen süre: " + usedTime + " saniye\n" +
+                "Skor: " + score,
+                "Oyun Bitti",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Önce resim seçin.");
+                return;
+            }
+
+            if (correctPieces == null)
+            {
+                MessageBox.Show("Önce puzzle oluşturun.");
+                return;
+            }
+
+            timeLeft = totalTime;
+            lblTime.Text = "Time: " + timeLeft;
+
+            score = 0;
+            lblScore.Text = "Score: 0";
+
+            gameStarted = true;
+            timer1.Start();
+
+            MessageBox.Show("Oyun başladı.");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            lblTime.Text = "Time: " + timeLeft;
+
+            if (timeLeft <= 0)
+            {
+                timer1.Stop();
+                gameStarted = false;
+                lblTime.Text = "Time: 0";
+
+                MessageBox.Show(
+                    "Süre bitti!\nFinal skor: " + score,
+                    "Oyun Bitti",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+            }
+        }
+
+        private void picPreview_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
